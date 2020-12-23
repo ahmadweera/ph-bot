ac774973463d120042fbacabf8cf0404b08dd908
Ahmad Weera <ahmadt.weera@gmail.com>
2020-12-22 21:34:55 -0500

    Update endpoint to use new data point

--- nba.js
+++ nba.js
@@ -8,134 +8,71 @@ const moment = require('moment-timezone');
 const discord = require('discord.js');
 
 const date_format = 'YYYYMMDD';
+const user_date_format = 'MMM Do YYYY';
 
 class Request {
-    constructor(endpoint, arg) {
-        this.method = 'GET'
-        this.url = `https://api-nba-v1.p.rapidapi.com/${endpoint}/${arg}`;
-        this.headers = {
-            'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-            'x-rapidapi-key': process.env.NBA_API_KEY,
-            'useQueryString': true
-        };
+    constructor(arg) {
+        this.method = 'GET';
+        this.url = `http://data.nba.net/prod/v1/${arg}/scoreboard.json`;
     }
 }
 
 module.exports = {
-    // GetGamesForDate: async function (arg) {
-    //     if (!arg) {
-    //         arg = moment().format(date_format);
-    //     }
+    GetGamesForDate: async function (arg) {
+        let embed = new discord.MessageEmbed();
+        let date = arg
+            ? moment(new Date(arg))
+            : moment().tz("America/Toronto");
 
-    //     let dates = GetRelativeDates(arg);
-    //     let trueDate = dates.d1;
-    //     let date = trueDate === moment().format(date_format) ? 'Today' : trueDate;
+        if (date.isValid()) {
+            const resp = await axios(new Request(date.format(date_format)))
+                .catch(async function (error) {
+                    console.log('API Error, Couldnt Process Date');
+                });
 
-    //     // send requests 
-    //     let responses = await axios.all([
-    //         axios(new Request('games/date', dates.d1)),
-    //         axios(new Request('games/date', dates.d1))
-    //     ]);
+            let games = resp
+                ? resp.data.games
+                : [];
 
-    //     let games = [];
-    //     responses.forEach((resp) => {
-    //         games = games.concat(resp.data.api.games)
-    //     });
+            embed = new discord.MessageEmbed();
+            embed.setTitle(`Games for ${date.format(user_date_format)}`);
 
-    //     games = games.filter(g => moment.utc(g.startTimeUTC).tz("America/Toronto").format(date_format) === trueDate);
+            games.forEach((game) => {
+                let name = '';
+                let value = '';
 
-    //     let embed = new discord.MessageEmbed();
-    //     embed.setTitle(`Games for ${date}`);
+                if (game.statusNum === 1) {
+                    name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
+                    value = "`" + game.startTimeEastern + "`";
+                }
 
-    //     games.forEach((game) => {
-    //         let name = '';
-    //         let value = '';
-    //         let ht = game.hTeam;
-    //         let at = game.vTeam;
+                else if (game.statusNum === 2) {
+                    name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
+                    value = `${game.hTeam.score} - ${game.vTeam.score} ` + "`";
 
-    //         if (game.statusGame === 'Scheduled') {
-    //             let time = new Date(game.startTimeUTC).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                    value += game.period.isHalftime
+                        ? ' HT'
+                        : `${game.clock} [Q${game.period.current}]`;
 
-    //             name = `${ht.fullName} @ ${at.fullName}`;
-    //             value = "`" + time + "`";
-    //         }
+                    value += "`"
+                }
 
-    //         else if (game.statusGame === 'In Play') {
-    //             name = `${ht.fullName} @ ${at.fullName}`;
-    //             value = `${ht.score.points} - ${at.score.points}  ` + "`" + ` Q${game.currentPeriod[0]} => ${game.clock} ` + "`";
-    //         }
+                else if (game.statusNum === 3) {
+                    name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
+                    value = `${game.hTeam.score} - ${game.hTeam.score} ` + "`FINAL`";
+                }
 
-    //         else if (game.statusGame === 'Finished') {
-    //             name = `${ht.fullName} @ ${at.fullName}`;
-    //             value = `${ht.score.points} - ${at.score.points} ` + "`FINAL`";
-    //         }
+                embed.addField(name, value, false);
+            });
 
-    //         embed.addField(name, value, false);
-    //     });
+            if (games.length === 0) {
+                embed.setDescription('No games scheduled');
+            }
 
-    //     if (games.length === 0) {
-    //         embed.setDescription(`No games scheduled for ${date}`);
-    //     }
-    //     return embed;
-    // },
-
-    GetGamesForDate: async function () {
-        let arg = moment.utc().subtract(5, 'hours').format(date_format);
-
-        let req = {
-            'method': 'GET',
-            'url': `http://data.nba.net/5s/prod/v1/${arg}/scoreboard.json`,
+            return embed;
         }
 
-        const resp = await axios(req);
-        const games = resp.data.games;
-
-        let embed = new discord.MessageEmbed();
-        embed.setTitle(`Games for today`);
-
-        games.forEach((game) => {
-            let name = '';
-            let value = '';
-
-            if (game.statusNum === 1) {
-                name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
-                value = "`" + game.startTimeEastern + "`";
-            }
-
-            else if (game.statusNum === 2) {
-                name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
-                value = `${game.hTeam.score} - ${game.vTeam.score}  `;
-
-                value += game.period.isHalftime
-                    ? ' HT'
-                    : ` (Q${game.period.current}) ${game.clock} `;
-            }
-
-            else if (game.statusNum === 3) {
-                name = `${game.hTeam.triCode} @ ${game.vTeam.triCode}`;
-                value = `${game.hTeam.score} - ${game.hTeam.score} ` + "`FINAL`";
-            }
-
-            embed.addField(name, value, false);
-        });
-
+        embed.setDescription('Invalid Date');
         return embed;
     }
-}
-
-function GetRelativeDates(dateStr) {
-    let d1 = IsNullOrEmpty(dateStr)
-        ? moment().format(date_format)
-        : moment(dateStr).format(date_format);
-
-    let d2 = moment(d1).add(1, 'days').format(date_format);
-
-    return {
-        'd1': d1,
-        'd2': d2
-    };
-}
-
-function IsNullOrEmpty(str) {
-    return (!str || str === '');
 }
