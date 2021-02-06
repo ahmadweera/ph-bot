0f24853f5041d292cf3050ed82f3f7cfd9b76652
Ahmad Weera <ahmadt.weera@gmail.com>
2021-02-05 22:38:29 -0500

    Simplify bot using slash commands; Delete youtube

--- bot.js
+++ bot.js
@@ -6,10 +6,8 @@ require('dotenv').config();
 const discord = require('discord.js');
 const nba = require('./nba')
 const spotify = require('./spotify')
-const yt = require('./youtube.js');
 
 const client = new discord.Client();
-const commands = ['help', 'new', 'nba', 'track', 'album'];
 
 client.once('ready', () => {
     console.log('client ready\n');
@@ -37,108 +35,16 @@ client.ws.on("INTERACTION_CREATE", async interaction => {
             break;
     }
 
-    channel.send(`${interaction.data.name}: ${option.value}\n${message}`)
-        .catch(console.error);
+    console.log('user: ' + interaction.member.user.username);
+    console.log('command: ' + interaction.data.name);
+    console.log('argument: ' + option.value + '\n');
+
+    channel.send(`<@${interaction.member.user.id}> ${interaction.data.name}: ${option.value}\n${message}`)
 });
 
 client.on('message', async (message) => {
-    if (ProcessMessage(message)) {
-        let content = message.content;
-        let command = RetrieveCommand(content);
-        let arg = RetrieveArgument(content);
-
-        console.log('user: ' + message.author.username);
-        console.log('command: ' + command);
-        console.log('argument: ' + arg + '\n');
-
-        await RunCommand(command, arg, message.channel);
-        VerifySuccess(message);
-    }
+    // todo do something
 });
-
-function ProcessMessage(message) {
-    content = message.content.toLowerCase();
-    if (!message.author.bot) {
-        for (let i = 0; i <= commands.length; i++) {
-            let command = commands[i];
-            if (content.startsWith(command)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    return false;
-}
-
-function RetrieveCommand(content) {
-    let ret = null;
-    content = content.toLowerCase();
-    let words = content.split(' ');
-
-    if (content && words[0]) {
-        ret = words[0];
-    }
-
-    return ret;
-}
-
-function RetrieveArgument(content) {
-    let ret = null;
-    content = content.trim();
-
-    if (content && content.split(' ').length > 1) {
-        ret = content.substr(content.indexOf(" ") + 1);
-    }
-
-    return ret;
-}
-
-async function RunCommand(command, arg, channel) {
-    let message = await GenerateMessage(command, arg);
-
-    if (message) {
-        switch (command.toLowerCase()) {
-            case 'nba':
-                await channel.send(message, { files: ['screenshots/scores.png'] });
-                break;
-            case 'new':
-                await channel.send(message).then(async msg => {
-                    await msg.pin();
-                    await ManagePins(channel);
-                });
-                break;
-            default:
-                await channel.send(message);
-                break;
-        }
-    }
-}
-
-async function GenerateMessage(command, arg) {
-    switch (command.toLowerCase()) {
-        case 'nba':
-            return await nba.GetGamesForDate(arg);
-        case 'new':
-            return await spotify.GetArtistNewRelease(arg);
-        case 'track':
-            return await spotify.GetItemByTitle(command, arg);
-        case 'album':
-            return await spotify.GetItemByTitle(command, arg);
-        case 'yt':
-            return await yt.GetVideoByKeyword(arg);
-    }
-}
-
-async function AddReactions(message, command) {
-    switch (command.toLowerCase()) {
-        case 'new':
-            message.react('👍');
-            message.react('👎');
-            break;
-    }
-}
 
 async function ManagePins(channel) {
     const limit = 3;
